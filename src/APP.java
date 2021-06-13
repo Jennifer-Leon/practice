36b968174565acf7440bb076e5085971d544367f
jennifer-leon <jennifer.anne.leon@gmail.com>
2021-06-13 08:36:12 -0700

    added initial code

---
+++ src/APP.java
@@ -0,0 +1,27 @@
+
+public class APP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// A about me section to allow for an initial practice
+	
+	String firstName = "Jennifer";
+	String schoolAttended = "ASU";
+	String degreeAttained = "BAS in Business";
+	int age = 34;
+	double yearsInIt = 3.5;
+	
+	
+	System.out.println("Name: " + firstName);
+	System.out.println("College Attended: " + schoolAttended);
+	System.out.println("Degree earned: " + degreeAttained);
+    System.out.println("Age: " + age);
+    System.out.println("How many years have you been in IT: " + yearsInIt);
+    
+	
+		
+	
+
+	}
+
+}
